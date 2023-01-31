b9a4f6757cef71e943b3d922751e9604374e4cdf
oksang <oksang@salin.co.kr>
2023-01-31 23:36:09 +0900

    feat : 디자인 수정 및 스케줄 계산

--- src/components/SettingPage.tsx
+++ src/components/SettingPage.tsx
@@ -1,46 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { add, format, parse } from "date-fns";
 import { TextField } from "@mui/material";
 
 function SettingPage() {
   const [scheduleSetData, setScheduleSetData] = useState({
-    defaultfDate: "",
-    scheduleName: "",
+    defaultDate: "20230102",
+    checkDate: format(new Date(), "yyyyMMdd"),
   });
+  const [scheduleName, setScheduleName] = useState("");
+
+  const sceduleCheck = () => {
+    const parseDefaultDate = parse(
+      scheduleSetData?.defaultDate,
+      "yyyyMMdd",
+      new Date()
+    );
+    const parseCheckDate = parse(
+      scheduleSetData?.checkDate,
+      "yyyyMMdd",
+      new Date()
+    );
+
+    console.log("parseCheckDate ::", parseCheckDate);
+
+    const diffDate = parseDefaultDate.getTime() - parseCheckDate.getTime();
+
+    const dataList = ["주간", "야간", "비번", "휴무"];
+
+    const diffDay = Math.abs(diffDate / (1000 * 60 * 60 * 24));
+
+    const isVal = Math.abs(parseDefaultDate.getDay() + 1 - 4);
+
+    const result =
+      parseCheckDate < parseDefaultDate ? (diffDay + 2) % 4 : diffDay % 4;
+
+    setScheduleName(dataList[result]);
+  };
 
   const onChange = (e: any, type: string) => {
-    if (type === "defaultfDate") {
-      const value = format(e, "yyyyMMdd");
-      setScheduleSetData({ ...scheduleSetData, [type]: value });
-    }
-    // const { value } = e.target;
+    const value = format(e, "yyyyMMdd");
+    setScheduleSetData({ ...scheduleSetData, [type]: value });
   };
+
+  useEffect(() => {
+    sceduleCheck();
+  }, [scheduleSetData]);
 
   return (
     <div>
-      <div>
-        <MobileDatePicker
-          label="지정일"
-          inputFormat="yyyy-MM-dd"
-          value={parse(scheduleSetData?.defaultfDate, "yyyyMMdd", new Date())}
-          mask="____-__-__"
-          onChange={(day) => onChange(day, "defaultfDate")}
-          renderInput={(params) => (
-            <TextField className="datePickerText" size="small" {...params} />
-          )}
-        />
-        <input
-          type="date"
-          onChange={(e) => onChange(e, "scheduleName")}
-          value={scheduleSetData?.scheduleName}
-        />
+      <div style={{ marginTop: "100px" }}>
+        <div>
+          <MobileDatePicker
+            label="지정일"
+            inputFormat="yyyy-MM-dd"
+            value={parse(scheduleSetData?.defaultDate, "yyyyMMdd", new Date())}
+            mask="____-__-__"
+            onChange={(day) => onChange(day, "defaultDate")}
+            renderInput={(params) => (
+              <TextField className="datePickerText" size="small" {...params} />
+            )}
+          />
+        </div>
+        <div style={{ marginTop: "10px" }}>
+          <MobileDatePicker
+            label="확인 날짜"
+            inputFormat="yyyy-MM-dd"
+            value={parse(scheduleSetData?.checkDate, "yyyyMMdd", new Date())}
+            mask="____-__-__"
+            onChange={(day) => onChange(day, "checkDate")}
+            renderInput={(params) => (
+              <TextField className="datePickerText" size="small" {...params} />
+            )}
+          />
+        </div>
       </div>
       {/* <div>
         확인 날짜 <input type="date" onChange={onChangeDate}></input>
       </div> */}
-      {/* <h1>{schedule}</h1> */}
-      <button>확인</button>
+      <h1>{scheduleName}</h1>
+      {/* <button onClick={sceduleCheck}>확인</button> */}
     </div>
   );
 }
